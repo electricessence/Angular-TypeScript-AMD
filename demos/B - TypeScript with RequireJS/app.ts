eee04f6c1bcb9420a64b65c89d764ed10c02112d
electricessence <oren@electrified.net>
2015-10-03 12:22:44 -0700

    Tweaks and code style settings.

--- demos/B - TypeScript with RequireJS/app.ts
+++ demos/B - TypeScript with RequireJS/app.ts
@@ -6,19 +6,18 @@ import controllers = require('./app.controllers');
 
 // We can prepare the application for bootstrapping before the DOM is ready.
 var appModule = angular
-	// Initialize the module.
-	.module('TypeScriptRequireJsDemo', [])
-	// Inject all the immediate controllers.
-	.controller(controllers);
+		.module('TypeScriptRequireJsDemo', []) // Initialize the module.
+		.controller(controllers) // Inject all the immediate controllers.
+	;
 
 // Defer bootstrapping until document is ready, and sub component is loaded.
-require(['./fieldset/component','domready!'],(FieldSet)=>{
+require(['./fieldset/component', 'domready!'], (FieldSet)=>
+{
 
 	FieldSet.activate(appModule);
 	angular.bootstrap(document, [appModule.name]);
 
 });
-
 
 
 // Deferred dependencies can then call for this module to append to it.
